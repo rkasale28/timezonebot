63af2e13f10b515e4cbe6eb9fad0c36ddc1a463c
rkasale28 <rohitkasale28@gmail.com>
2020-06-07 15:50:36 +0530

    Added Actions

--- actions.py
+++ actions.py
@@ -7,21 +7,35 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+
+class ActionShowTimeZone(Action):
+
+    def name(self) -> Text:
+        return "action_show_time_zone"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        
+        timezone = {
+            "London":"UTC+1:00",
+            "Sofia":"UTC+3:00"
+            "Lisbon":"UTC+1:00",
+            "Mumbai":"UTC+5:30",
+        }
+
+        city = tracker.get_slot("city")
+        timezones = timezone.get(city)
+
+        if timezones is None:
+            output = "Could not find time zone of {}".format(city)
+        else: 
+           output = "Time zone of {} is {}".format(city, timezones)
+     
+        dispatcher.utter_message(text=output)
+
+        return []
